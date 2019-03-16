333feffbaf417d8ca33b19676b2d047ea2121081
tacur <36847088+tacur@users.noreply.github.com>
2019-03-16 17:01:09 +0100

    s

--- js/main.js
+++ js/main.js
@@ -60,66 +60,84 @@ jQuery(document).ready(function($){
 		resize = false;
 	}
 });
-
 jQuery(document).ready(function($){
-	//cache some jQuery objects
-	var modalTrigger = $('.cd-modal-trigger1'),
-		transitionLayer = $('.cd-transition-layer'),
-		transitionBackground = transitionLayer.children(),
-		modalWindow = $('.cd-modal1');
+	//trigger the animation - open modal window
+	$('[data-type="modal-trigger"]').on('click', function(){
+		var actionBtn = $(this),
+			scaleValue = retrieveScale(actionBtn.next('.cd-modal-bg'));
+		
+		actionBtn.addClass('to-circle');
+		actionBtn.next('.cd-modal-bg').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+			animateLayer(actionBtn.next('.cd-modal-bg'), scaleValue, true);
+		});
 
-	var frameProportion = 1.78, //png frame aspect ratio
-		frames = 25, //number of png frames
-		resize = false;
+		//if browser doesn't support transitions...
+		if(actionBtn.parents('.no-csstransitions').length > 0 ) animateLayer(actionBtn.next('.cd-modal-bg'), scaleValue, true);
+	});
 
-	//set transitionBackground dimentions
-	setLayerDimensions();
+	//trigger the animation - close modal window
+	$('.cd-section .cd-modal-close').on('click', function(){
+		closeModal();
+	});
+	$(document).keyup(function(event){
+		if(event.which=='27') closeModal();
+	});
+
 	$(window).on('resize', function(){
-		if( !resize ) {
-			resize = true;
-			(!window.requestAnimationFrame) ? setTimeout(setLayerDimensions, 300) : window.requestAnimationFrame(setLayerDimensions);
-		}
+		//on window resize - update cover layer dimention and position
+		if($('.cd-section.modal-is-visible').length > 0) window.requestAnimationFrame(updateLayer);
 	});
 
-	//open modal window
-	modalTrigger.on('click', function(event){	
-		event.preventDefault();
-		transitionLayer.addClass('visible opening');
-		var delay = ( $('.no-cssanimations').length > 0 ) ? 0 : 600;
-		setTimeout(function(){
-			modalWindow.addClass('visible');
-		}, delay);
-	});
+	function retrieveScale(btn) {
+		var btnRadius = btn.width()/2,
+			left = btn.offset().left + btnRadius,
+			top = btn.offset().top + btnRadius - $(window).scrollTop(),
+			scale = scaleValue(top, left, btnRadius, $(window).height(), $(window).width());
 
-	//close modal window
-	modalWindow.on('click', '.modal-close', function(event){
-		event.preventDefault();
-		transitionLayer.addClass('closing');
-		modalWindow.removeClass('visible');
-		transitionBackground.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
-			transitionLayer.removeClass('closing opening visible');
-			transitionBackground.off('webkitAnimationEnd oanimationend msAnimationEnd animationend');
+		btn.css('position', 'fixed').velocity({
+			top: top - btnRadius,
+			left: left - btnRadius,
+			translateX: 0,
+		}, 0);
+
+		return scale;
+	}
+
+	function scaleValue( topValue, leftValue, radiusValue, windowW, windowH) {
+		var maxDistHor = ( leftValue > windowW/2) ? leftValue : (windowW - leftValue),
+			maxDistVert = ( topValue > windowH/2) ? topValue : (windowH - topValue);
+		return Math.ceil(Math.sqrt( Math.pow(maxDistHor, 2) + Math.pow(maxDistVert, 2) )/radiusValue);
+	}
+
+	function animateLayer(layer, scaleVal, bool) {
+		layer.velocity({ scale: scaleVal }, 400, function(){
+			$('body').toggleClass('overflow-hidden', bool);
+			(bool) 
+				? layer.parents('.cd-section').addClass('modal-is-visible').end().off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend')
+				: layer.removeClass('is-visible').removeAttr( 'style' ).siblings('[data-type="modal-trigger"]').removeClass('to-circle');
 		});
-	});
+	}
 
-	function setLayerDimensions() {
-		var windowWidth = $(window).width(),
-			windowHeight = $(window).height(),
-			layerHeight, layerWidth;
+	function updateLayer() {
+		var layer = $('.cd-section.modal-is-visible').find('.cd-modal-bg'),
+			layerRadius = layer.width()/2,
+			layerTop = layer.siblings('.btn').offset().top + layerRadius - $(window).scrollTop(),
+			layerLeft = layer.siblings('.btn').offset().left + layerRadius,
+			scale = scaleValue(layerTop, layerLeft, layerRadius, $(window).height(), $(window).width());
+		
+		layer.velocity({
+			top: layerTop - layerRadius,
+			left: layerLeft - layerRadius,
+			scale: scale,
+		}, 0);
+	}
 
-		if( windowWidth/windowHeight > frameProportion ) {
-			layerWidth = windowWidth;
-			layerHeight = layerWidth/frameProportion;
-		} else {
-			layerHeight = windowHeight*1.2;
-			layerWidth = layerHeight*frameProportion;
-		}
-
-		transitionBackground.css({
-			'width': layerWidth*frames+'px',
-			'height': layerHeight+'px',
+	function closeModal() {
+		var section = $('.cd-section.modal-is-visible');
+		section.removeClass('modal-is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+			animateLayer(section.find('.cd-modal-bg'), 1, false);
 		});
-
-		resize = false;
+		//if browser doesn't support transitions...
+		if(section.parents('.no-csstransitions').length > 0 ) animateLayer(section.find('.cd-modal-bg'), 1, false);
 	}
 });
